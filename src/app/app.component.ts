f9f6f56ca5c53f26edb66ab7312fae8d8b6f339b
Stan <stygia86@gmail.com>
2021-10-24 22:55:48 +0200

    First comm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'condor-app';
+export class AppComponent implements
+OnInit {
+  name = 'Pinkman';
+
+constructor(
+    private title: Title,
+    private meta: Meta
+) {}
+
+ngOnInit() {
+    this.title.setTitle('Pinkman');
+    this.meta.updateTag({ name: 'description', content: 'Pinkman' });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { CareerComponent } from './career/career.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServicesComponent } from './services/services.component'
+
+import { MatIconModule } from '@angular/material/icon'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ContactComponent } from './contact/contact.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    AboutComponent,
+    FooterComponent,
+    CareerComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
